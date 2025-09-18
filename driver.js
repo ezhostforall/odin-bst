b44591d9982d1d1f801a4054788d05363c8e07a0
ezhostforall <117274922+ezhostforall@users.noreply.github.com>
2025-09-18 21:06:38 +0000

    add traversal methods output to binary search tree driver script

--- driver.js
+++ driver.js
@@ -18,3 +18,25 @@ console.log("\nConstructed Balanced BST:");
 tree.prettyPrint();
 
 console.log("\nIs tree balanced?", tree.isBalanced());
+
+console.log("\n=== Traversal Methods ===");
+
+console.log("\nLevel order:");
+const levelOrder = [];
+tree.levelOrderForEach(node => levelOrder.push(node.data));
+console.log(levelOrder.join(', '));
+
+console.log("\nPre order:");
+const preOrder = [];
+tree.preOrderForEach(node => preOrder.push(node.data));
+console.log(preOrder.join(', '));
+
+console.log("\nPost order:");
+const postOrder = [];
+tree.postOrderForEach(node => postOrder.push(node.data));
+console.log(postOrder.join(', '));
+
+console.log("\nIn order:");
+const inOrder = [];
+tree.inOrderForEach(node => inOrder.push(node.data));
+console.log(inOrder.join(', '));
