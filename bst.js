28e91b925e521c5204030e228a96712a5e811b3c
ezhostforall <117274922+ezhostforall@users.noreply.github.com>
2025-09-18 20:38:48 +0000

    implement buildTree and _buildTreeHelper methods in Tree class for constructing a binary search tree

--- bst.js
+++ bst.js
@@ -10,4 +10,23 @@ class Tree {
   constructor(array) {
     this.root = this.buildTree(array);
   }
+
+  buildTree(array) {
+    if (!array.length) return null;
+
+    const dedupedSortedArray = [...new Set(array)].sort((a, b) => a - b);
+    return this._buildTreeHelper(dedupedSortedArray);
+  }
+
+  _buildTreeHelper(array) {
+    if (!array.length) return null;
+
+    const midIndex = Math.floor(array.length / 2);
+    const node = new Node(array[midIndex]);
+
+    node.left = this._buildTreeHelper(array.slice(0, midIndex));
+    node.right = this._buildTreeHelper(array.slice(midIndex + 1));
+
+    return node;
+  }
 }
